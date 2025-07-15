cc263e4cfb140715620f578c94afef19b7fed8ae
TosettoFD <facundo.tosetto@est.fi.uncoma.edu.ar>
2025-07-15 14:04:10 -0300

    confirmo,anda

--- src/main/TransporteDeAgua.java
+++ src/main/TransporteDeAgua.java
@@ -11,7 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TransporteDeAgua {
-    GrafoEtiquetado map = new GrafoEtiquetado();
+    GrafoEtiquetado mapa = new GrafoEtiquetado();
 
     public static void main(String[] args) {
 
